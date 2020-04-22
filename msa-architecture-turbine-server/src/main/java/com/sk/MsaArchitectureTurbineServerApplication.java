edbbb31239d76678d5c7869546e23393f9794d8c
kyungje <kyungje@gmail.com>
2020-04-22 20:06:27 +0900

    init

---
+++ msa-architecture-turbine-server/src/main/java/com/sk/MsaArchitectureTurbineServerApplication.java
@@ -0,0 +1,17 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbine
+public class MsaArchitectureTurbineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaArchitectureTurbineServerApplication.class, args);
+	}
+
+}
